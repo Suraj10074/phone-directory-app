579d21c6270bd7a287c4af925840a37e74d1d954
Suraj <surajthakur10074@gmail.com>
2020-07-21 00:26:14 +0530

    Styled ‘App’ (External Styling)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import Header from './Header'
+import Header from './Header';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <button>Add</button>
-          <div>
-            <br />  
-            <span>Name</span>
-            <br />
-            <span>Phone</span>
-          </div>
+        <Header/>
+        <div className="component-body-container">
+            <button className="custom-btn add-btn">Add</button>
+            <div className="grid-container heading-container">
+                <span className="grid-item name-heading">Name</span>
+                <span className="grid-item name-heading">Phone</span>
+            </div>
+        </div>
       </div>
     );
   }
