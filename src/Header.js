dbfec511485e64cb27c7a1a57867ba229c1d7ff6
Suraj <surajthakur10074@gmail.com>
2020-07-20 21:34:14 +0530

    Styled ‘Header’ (Inline Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
-const Header = () =>{
-    return (
-        <div className="header">
+const Header = () => {
+  // Example (Inline Styling):
+
+
+  // Approach 1 - Directly writing style alongside JSX:
+
+  /* return(
+      <div style={{textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase'}}>
         Phone Directory
-      </div> 
+      </div>
     )
+  */
+
+  // Approach 2 - Moving style as constant object:
+  const headerStyle = {
+    textAlign: 'center',
+    padding: 20,
+    background: '#000',
+    color: '#fff',
+    textTransform: 'uppercase'
+  }
+
+  return (
+    <div style={headerStyle}>
+      Phone Directory
+    </div>
+  )
+
 }
 export default Header;
