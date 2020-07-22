0f82edc2f0105c77ee04cd7cfecbed94edb718d5
Suraj <surajthakur10074@gmail.com>
2020-07-23 04:30:23 +0530

    Updates

---
+++ precache-manifest.7ec21485440d306cfa0ea8fc37bf79a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40efb69583987f41ada77e4e335c5254",
+    "url": "/phone-directory-app/index.html"
+  },
+  {
+    "revision": "8055a1c64d44bffc56d5",
+    "url": "/phone-directory-app/static/css/main.42f965f2.chunk.css"
+  },
+  {
+    "revision": "f2b82d85cea8e790d7ef",
+    "url": "/phone-directory-app/static/js/2.cc64363c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phone-directory-app/static/js/2.cc64363c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8055a1c64d44bffc56d5",
+    "url": "/phone-directory-app/static/js/main.1a26a71e.chunk.js"
+  },
+  {
+    "revision": "6ff874e0e66b39ceb0b5",
+    "url": "/phone-directory-app/static/js/runtime-main.1e15a315.js"
+  }
+]);
